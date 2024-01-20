f872282b636fb1bcf930f6fe35a3f54de3699d26
erdo <226319+erdo@users.noreply.github.com>
2024-01-20 11:34:20 +0100

    kotlin 1.9.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.22")
     }
 }
 
