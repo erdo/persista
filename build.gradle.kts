5eef8d725f62871fd93514ab7f062dbe87cb8a52
erdo <226319+erdo@users.noreply.github.com>
2024-01-27 10:44:55 +0100

    AGP 8.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.22")
     }
