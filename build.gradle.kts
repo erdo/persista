ff841fc341b3fb3d39702a3c77c2d17fbec7c97e
erdo <226319+erdo@users.noreply.github.com>
2024-04-11 22:10:53 +0100

    bump dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,12 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("com.android.tools.build:gradle:8.3.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.22")
     }
 }
 
 tasks.register("clean", Delete::class){
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
