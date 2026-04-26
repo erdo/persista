a6d7b00cfd27c8e098a95cb557581855e4a3f932
erdo <226319+erdo@users.noreply.github.com>
2026-04-26 09:22:54 +0200

    nmcp publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import co.early.persista.Shared
+
 plugins {
     alias(libs.plugins.androidAppPlugin).apply(false)
     alias(libs.plugins.androidLibraryPlugin).apply(false)
@@ -6,4 +8,17 @@ plugins {
     alias(libs.plugins.kotlinMultiPlatformPlugin).apply(false)
     alias(libs.plugins.kotlinSerializationPlugin).apply(false)
     alias(libs.plugins.kotlinCocoapodsPlugin).apply(false)
+    id("com.gradleup.nmcp.aggregation")
+}
+
+nmcpAggregation {
+    centralPortal {
+        username = Shared.Secrets.MAVEN_USER
+        password = Shared.Secrets.MAVEN_PASSWORD
+        publishingType = "USER_MANAGED" // USER_MANAGED | AUTOMATIC
+    }
+}
+
+dependencies {
+    nmcpAggregation(project(":persista"))
 }
